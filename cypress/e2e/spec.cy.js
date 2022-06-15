69f6c88f15b82ef376300d94a1c3296d425f9998
Jörg Plewe <joerg.arno.plewe@externe.dfs.de>
2022-06-15 18:05:26 +0200

    create screenshot

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,14 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
-  })
+  });
+
+  it("creates a screenshot", () => {
+    // Screenshot is stored in different locations,
+    // depending on whether `cypress:open` or `cypress:run`
+    // is executed.
+    // Expected: `~/cypress/screenshots/spec.cy.js/myScreenshot.png`
+    // Found (with `cypress:open`): `~/cypress/screenshots/myScreenshot.png` 
+    cy.screenshot("myScreenshot")
+  });
 })
